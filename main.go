86bbb58a8b4ac6e8b094cef3ebdcb7290a4af215
dreinix <ricardo.aar@outlook.com>
2021-04-18 22:36:36 -0400

    "Added flowControl practice"

--- main.go
+++ main.go
@@ -2,19 +2,40 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
-
-	"github.com/dreinix/GoTour/variableTest"
+	"runtime"
 )
 
+func Sqrt(x float64) float64 {
+	var z float64
+	if x < 10 { //10 = 2*3+1
+		z = float64(3 * x)
+	} else {
+		z = float64(x / 3) // factor de 3
+	}
+
+	var tmp float64 = z * 2
+	for tmp-z > 0.00000001 { // 6 decimales aceptables.
+		tmp = z
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+
+}
+
+func swtch() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+}
+
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(50))
-	//x := 4.68
-	fmt.Printf("I have the sqare of %.3g and thats pi -> %.5f \n", math.Sqrt(12), math.Pi)
-	a, x := variableTest.Swap("inix", "hola")
-
-	println(a)
-	println(x)
-
+	swtch()
 }
